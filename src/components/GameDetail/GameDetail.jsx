16207d49762f909fc43506b344452a4775d8f002
Paola Aguilar <pao.aquev29@gmail.com>
2021-03-20 20:23:36 -0600

    💄 adding styles to the game detail component

--- src/components/GameDetail/GameDetail.jsx
+++ src/components/GameDetail/GameDetail.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import "../../components/GameDetail/gameDetail.scss";
 
 const GameDetail = ({ game }) => {
   return (
     <div>
       <h1>GAME DETAIL</h1>
-      <div className="movie__container">
-        <img alt="movie" src={game.image} />
-        <div className="movie__card-body">
-          <p>{game.title}</p>
-          <p>{game.createDate}</p>
+      <div className="game">
+        <div className="game__container">
+          <h1 className="game__title">{game.title}</h1>
+          <h2 className="game__subtitle">{game.subTitle}</h2>
+          <img className="game__image" alt="" src={game.image} />
+          <div className="game__date">
+            <h5>Date of creation: {game.createDate}</h5>
+          </div>
+          <br />
+          <div>
+            <h5>{game.body}</h5>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ListOfGames/ListOfGames.jsx
+++ src/components/ListOfGames/ListOfGames.jsx
@@ -4,38 +4,40 @@ import "../ListOfGames/listOfGames.scss";
 
 const ListOfGames = ({ changePage, changeGame }) => {
   const [gameList, setGameList] = useState([]);
-  const [page, setPage] = useState();
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     console.log("Use effect");
     getData().then((result) => {
       setGameList(result);
     });
-  }, []);
+  }, [page]);
 
   const getData = async () => {
     try {
-      const res = await fetch("http://localhost:3000/posts?_page=1");
+      const res = await fetch(`http://localhost:3000/posts?_page=${page}`);
       const data = await res.json();
       return data;
     } catch (error) {
       console.log(error);
     }
   };
-
   return (
     <div>
-      <h1>LIST OF GAMES</h1>
+      <h1>LIST OF GAMES {page}</h1>
       <div className="movie">
         {gameList.map((game) => {
           return (
             <GameCard
+              key={game.id}
               changeGame={changeGame}
               changePage={changePage}
               game={game}
             />
           );
         })}
+        <button onClick={() => setPage(page - 1)}>Previous</button>
+        <button onClick={() => setPage(page + 1)}>Next</button>
       </div>
     </div>
   );
